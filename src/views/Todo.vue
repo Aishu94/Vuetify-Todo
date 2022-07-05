c92422f4916f29472809cb0f73173f4b16b183fe
Aishwarya Dongare <aishwaryadongare.pune@gmail.com>
2022-07-05 17:21:55 +1000

    Added "Delete" Property(icon-click)

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="home">
+    <v-text-field class="pa-3" outlined label="Add Task" append-icon="mdi-plus" hide-details clearable></v-text-field>
     <v-list flat class="pt-0">
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item 
-        @click="doneTask(task.id)"
-        :class="{'blue lighten-5' : task.done}"
-        >
+        <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5': task.done }">
           <template v-slot:default>
             <v-list-item-action>
               <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
             </v-list-item-action>
 
             <v-list-item-content>
-              <v-list-item-title
-              :class="{'text-decoration-line-through' : task.done}"
-              >{{ task.title }}</v-list-item-title>
+              <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title }}
+              </v-list-item-title>
             </v-list-item-content>
+            <v-list-item-action>
+              <v-btn icon @click.stop="deleteTask(task.id)">
+                <v-icon color="primary lighten-1">mdi-delete-circle</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
+
         </v-list-item>
         <v-divider></v-divider>
       </div>
@@ -52,12 +55,15 @@ export default {
     }
   },
   methods: {
-      doneTask(id) {
-        // console.log('id:', id);
-        let task = this.tasks.filter(task => task.id === id) [0]
-        task.done = !task.done
-      }
+    doneTask(id) {
+      // console.log('id:', id);
+      let task = this.tasks.filter(task => task.id === id)[0]
+      task.done = !task.done
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
     }
+  }
 
 
 }
