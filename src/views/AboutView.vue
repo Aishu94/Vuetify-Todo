63f7532d92588f11f87946f60936a26160102246
Aishwarya Dongare <aishwaryadongare.pune@gmail.com>
2022-07-05 17:04:32 +1000

    Added list with Vuetify Design components

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
   </div>
 </template>
---
+++ src/views/Todo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home">
+    <v-list flat class="pt-0">
+      <div v-for="task in tasks" :key="task.id">
+        <v-list-item 
+        @click="doneTask(task.id)"
+        :class="{'blue lighten-5' : task.done}"
+        >
+          <template v-slot:default>
+            <v-list-item-action>
+              <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title
+              :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+
+
+    </v-list>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: 'Wake up',
+          done: false
+        },
+        {
+          id: 2,
+          title: 'Eat',
+          done: false
+        },
+        {
+          id: 3,
+          title: 'Cycle',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+      doneTask(id) {
+        // console.log('id:', id);
+        let task = this.tasks.filter(task => task.id === id) [0]
+        task.done = !task.done
+      }
+    }
+
+
+}
+</script>
